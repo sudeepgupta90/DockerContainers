8d2e5a99bb7bcf116548a2cf44ccabc564fd462f
Sudeep Gupta <sudeepgupta90@gmail.com>
2018-05-24 12:04:02 +0000

    fixed ssh services start script and finalised docker ansible container

--- alp3.7_py3.5.5_docker18.03.1/configs/start.sh
+++ alp3.7_py3.5.5_docker18.03.1/configs/start.sh
@@ -1,4 +1,9 @@
+#!/bin/ash
+
 rc-update add sshd
-touch /run/openrc/softlevel
+#touch /run/openrc/softlevel
 /etc/init.d/sshd start
 echo "ssh started"
+#while true;do script -q -c "/bin/ash -l" /dev/null ; done && \
+#script -q -c "/bin/ash -l" /dev/null && \
+#script -q -c "tail -f /dev/null"
